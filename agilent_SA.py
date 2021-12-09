7eade1403c42d964799f9454f565e480997b3688
benny <benny08118@gmail.com>
2021-12-09 14:29:31 +0800

    add trace type

--- agilent_SA.py
+++ agilent_SA.py
@@ -1,8 +1,8 @@
 import pyvisa
 from matplotlib import pyplot as plt
 
-
 rm = pyvisa.ResourceManager()
+
 current_instr = "GPIB0::21::INSTR"
 # class
 inst = rm.open_resource(current_instr,send_end=False)
@@ -19,32 +19,32 @@ class Freq:
         self.instr = instr
     #set SA centFreq
 
-    def centFreq(self, Freq, unit="MHz"):
+    def cent_freq(self, Freq, unit="MHz"):
         responce = self.instr.write("FREQ:CENT " + Freq + unit)
         return responce
     #set SA startFreq
 
-    def startFreq(self, Freq, unit="MHz"):
+    def start_freq(self, Freq, unit="MHz"):
         responce = self.instr.write("FREQ:START " + Freq + unit)
         return responce
     #set SA stopFreq
 
-    def stopFreq(self, Freq, unit="MHz"):
+    def stop_freq(self, Freq, unit="MHz"):
         responce = self.instr.write("FREQ:STOP " + Freq + unit)
         return responce
     #get SA CentFreq
 
-    def getCentFreq(self):
+    def get_cent_freq(self):
         responce = self.instr.query("FREQ:CENT?")
         return responce
     #get SA StartFreq
 
-    def getStartFreq(self):
+    def get_start_freq(self):
         responce = self.instr.query("FREQ:START?")
         return responce
     #get SA StopFreq
 
-    def getStopFreq(self):
+    def get_stop_freq(self):
         responce = self.instr.query("FREQ:STOP?")
         return responce
 
@@ -54,17 +54,17 @@ class Span:
         self.instr = instr
 
     #set SA SPAN
-    def setSpan(self, Freq, unit="MHz"):
+    def set_span(self, Freq, unit="MHz"):
         responce = self.instr.write("FREQ:SPAN " + Freq + unit)
         return responce
 
     #get SA SPAN
-    def getSpan(self):
+    def get_span(self):
         responce = self.instr.query("FREQ:SPAN?")
         return responce
 
     #get SA SPAN
-    def setFullSpan(self):
+    def set_full_span(self):
         responce = self.instr.write("FREQ:SPAN:FULL")
         return responce
 
@@ -73,29 +73,29 @@ class Amptd:
     def __init__(self, instr):
         self.instr = instr
 
-    def setRefLeve(self, Amp, unit="dBm"):
+    def set_ref_leve(self, Amp, unit="dBm"):
         responce = self.instr.write("DISP:WIND:TRAC:Y:RLEV " + Amp + unit)
         return responce
 
-    def getRefLeve(self):
+    def get_ref_leve(self):
         responce = self.instr.query("DISP:WIND:TRAC:Y:RLEV?")
         return responce
 
     #set SA Attenuation
-    def setMechAtten(self, att):
+    def set_mech_atten(self, att):
         responce = self.instr.write("POW:ATT " + att)
         return responce
 
     #get SA Attenuation
-    def getMechAtten(self):
+    def get_mech_atten(self):
         responce = self.instr.query("POW:ATT?")
         return responce
 
-    def setMechAttenAuto(self):
+    def set_mech_attenAuto(self):
         responce = self.instr.write("POW:ATT:AUTO 1")
         return responce
 
-    def getMechAttenAuto(self):
+    def get_mech_atten_auto(self):
         responce = self.instr.query("POW:ATT:AUTO?")
         return responce
 
@@ -104,21 +104,30 @@ class Fetch:
     def __init__(self):
         pass
 
-    def fetchData(self): 
-    #
+    def fetch_data(self): 
     #FETC:SAN1?
-        self.instr.timeout = 550000
-        responce = self.instr.write("CALC: DATA?")
-        responce = self.instr.read()
+        self.instr.timeout = 100000
+        self.instr.read_termination = "\n"
+        responce = self.instr.query("CALC:DATA?")
+        # responce = self.instr.query("FETC:SAN1?")
         return responce
+
+    def fetch_chp(self):
+        pass
+        # self.instr.timeout = 100000
+        # self.instr.read_termination = "\n"
+        # responce = self.instr.query(":FETC:CHP?")
+        # return responce
+
 
 class TraceDetector:
     def __init__(self):
         pass
 
-    def setTraceType(self, trace='1', traceType='AVER'):
-        #trace={1...6} traceType=WRIT or AVER or MAXH ot MINH 
-        responce = self.instr.query("TRAC{}:TYPE {}".format(trace,traceType))
+    def set_trace_type(self, trace='1', traceType='AVER'):
+        # trace={1...6} traceType=WRIT or AVER or MAXH ot MINH 
+        #print("TRAC{}:TYPE {}".format(trace, traceType))
+        responce = self.instr.write("TRAC{}:TYPE {}".format(trace, traceType))
         return responce
 
 
@@ -134,23 +143,24 @@ class AgilentSA(AnalyzerSetUp):
 
 def main():
     new_instr = AgilentSA(inst)
-    new_instr.centFreq("5180")
-    print(float(new_instr.getStartFreq()))
-    print("getCentFreq:", float(new_instr.getCentFreq()), "Hz")
-    print("getStartFreq:", float(new_instr.getStartFreq()), "Hz")
-    print("getStopFreq:", float(new_instr.getStopFreq()), "Hz")
-    new_instr.setSpan('100')
-    print("getSpan:", float(new_instr.getSpan()), "Hz")
-    new_instr.setRefLeve('10')
-    print("getRefLeve:", float(new_instr.getRefLeve()), "dBm")
-    new_instr.setMechAtten('10')
-    print("getAttenuationStatus(1:ON,0:OFF):", new_instr.getMechAttenAuto())
-    print("getAttenuation:", float(new_instr.getMechAtten()), "dB")
-    new_instr.setTraceType()
+    new_instr.cent_freq("5180")
+    print(float(new_instr.get_start_freq()))
+    print("getCentFreq:", float(new_instr.get_cent_freq()), "Hz")
+    print("getStartFreq:", float(new_instr.get_start_freq()), "Hz")
+    print("getStopFreq:", float(new_instr.get_stop_freq()), "Hz")
+    new_instr.set_span('100')
+    print("getSpan:", float(new_instr.get_span()), "Hz")
+    new_instr.set_ref_leve('10')
+    print("getRefLeve:", float(new_instr.get_ref_leve()), "dBm")
+    new_instr.set_mech_atten('10')
+    print("getAttenuationStatus(1:ON,0:OFF):", new_instr.get_mech_atten_auto())
+    print("getAttenuation:", float(new_instr.get_mech_atten()), "dB")
+    new_instr.set_trace_type('1', 'MAXH')
     # new_instr.setMechAttenAuto()
     # print("getAttenuationStatus(1:ON,0:OFF):", new_instr.getMechAttenAuto())
     # print("getAttenuation:", float(new_instr.getMechAtten()), "dB")
-    trace = new_instr.fetchData()
+    trace = new_instr.fetch_data()
+    trace=trace.split(',')
     print(trace)
     amp = trace[1::2]
     freq = trace[::2]
@@ -164,6 +174,7 @@ def main():
         newFreq.append(float(f)/1000000)
     for a in amp:
         newAmp.append(float(a)+22)
+
     print(len(newFreq))
     print(len(newAmp))
 
